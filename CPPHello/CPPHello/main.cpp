e1e763a6fae4de9b0d07a6abc960754a9a767752
maxpyne1981 <sankha_s@yahoo.com>
2018-10-29 23:38:07 -0700

    first cpp xcode project

---
+++ CPPHello/CPPHello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPHello
+//
+//  Created by Sankha Subhra Sarkar on 10/29/18.
+//  Copyright © 2018 Sankha Subhra Sarkar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
